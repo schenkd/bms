994df9a3509f6891d4ae43eaf01a85dad5ea5672
David Schenk <davidschenk4@gmail.com>
2016-08-21 17:19:48 +0700

    :lock: new test & password hash
    preperation for login auth

--- app/models.py
+++ app/models.py
@@ -1,4 +1,12 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Role(db.Model):
@@ -12,11 +20,24 @@ class Role(db.Model):
         return '<Role %r' % self.name
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Optional: Gibt einen lesbaren String zurück für z.B. debugging oder testing
     def __repr__(self):
