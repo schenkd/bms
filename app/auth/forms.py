3cd3cfa8ccb6af1672b36a671daed8013e2e95b5
David Schenk <davidschenk4@gmail.com>
2016-08-22 13:47:28 +0700

    :registered: user registration works
    nice

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from ..models import User
 
 
 class LoginForm(Form):
@@ -10,3 +11,26 @@ class LoginForm(Form):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Login')
+
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[DataRequired(),
+                                             Length(min=1, max=64),
+                                             Email()])
+    username = StringField('Username', validators=[DataRequired(),
+                                                   Length(min=1, max=64),
+                                                   Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                          'Usernames must have only letters, '
+                                                          'numbers, dots or underscores')])
+    password = PasswordField('Password', validators=[DataRequired(),
+                                                     EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confrm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use')
