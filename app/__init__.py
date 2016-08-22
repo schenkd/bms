0913829f7234228ecba5b7c1c330d252eb80279b
David Schenk <davidschenk4@gmail.com>
2016-08-22 16:43:51 +0700

    account managment & error pages

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 
 moment = Moment()
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+mail = Mail()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -18,10 +20,12 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
     db.init_app(app)
     moment.init_app(app)
     login_manager.init_app(app)
     bootstrap.init_app(app)
+    mail.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- config.py
+++ config.py
@@ -12,6 +12,15 @@ class Config:
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+    # mail
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[sauerbraten]'
+    FLASKY_MAIL_SENDER = 'sauerbraten.de admim <admin@sauerbraten.de>'
+
     # properties
     FLASKY_POSTS_PER_PAGE = 3
 
