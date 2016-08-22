273e383272c081505f86bd402776d85baa54d42c
David Schenk <davidschenk4@gmail.com>
2016-08-22 12:42:33 +0700

    :monkey_face: db migration & login function
    - made a db migration for the new user model
    - User login works

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_moment import Moment
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 moment = Moment()
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -19,6 +21,7 @@ def create_app(config_name):
     db.init_app(app)
     moment.init_app(app)
     login_manager.init_app(app)
+    bootstrap.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
